524adc5ded2bfd231de8f33a6d59ec2c6b838fd1
Renziito <sepia.aki@gmail.com>
2019-08-11 12:23:58 -0500

    database

--- protected/config/database.php
+++ protected/config/database.php
@@ -2,7 +2,7 @@
 
 // This is the database connection configuration.
 return array(
-    'connectionString' => 'mysql:host=localhost;dbname=partnership',
+    'connectionString' => 'mysql:host=localhost;dbname=db_partnership',
     'emulatePrepare'   => true,
     'username'         => 'db_user',
     'password'         => 'u53rdb',
