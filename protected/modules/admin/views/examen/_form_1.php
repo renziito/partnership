bde7906e5d9071dd35d40bfc1815aced50170aec
Renziito <sepia.aki@gmail.com>
2019-09-17 00:41:45 -0500

    form 1

--- protected/modules/admin/views/examen/_form_1.php
+++ protected/modules/admin/views/examen/_form_1.php
@@ -23,7 +23,7 @@
                 <?= $form->labelEx($model, 'titulo'); ?>
                 <?=
                 $form->textField($model, 'titulo',
-                        ['class' => 'form-control']);
+                        ['class' => 'form-control', 'required' => 'required']);
                 ?>
             </div>
         </div>
@@ -49,7 +49,7 @@
                 $form->dropDownList($model, 'tipo_calificacion', [
                     1 => 'Nota', 'Puntaje', 'Promedio'
                         ], [
-                    'class' => 'form-control'
+                    'class'    => 'form-control', 'required' => 'required'
                 ])
                 ?>
             </div>
@@ -72,16 +72,40 @@
         <div class="col-xs-4">
             <div class="form-group form-group-default required">
                 <label for="cantidad">Cantidad de Preguntas</label>
-                <input class="form-control" step="any"  id="cantidad" type="number" value="0">
+                <input class="form-control" step="any"  id="cantidad" type="number" value="0" required>
             </div>
         </div>
     </div>
 
     <hr>
-    <?= CHtml::button('Continuar', ['class' => 'btn btn-success','id'=>'btn-continue']); ?>
+    <?= CHtml::button('Continuar', ['class' => 'btn btn-success', 'id' => 'btn-continue']); ?>
 
     <div id="preguntas">
     </div>
     <?php $this->endWidget(); ?>
 
 </div><!-- form -->
+
+<script>
+    $('#btn-continue').on('click', function () {
+        var preguntas = $('<div>');
+        var save = $('<input>').addClass('btn btn-success').attr('type', 'submit').attr('value', 'Guardar');
+        var cantidad = $('#cantidad').val();
+
+        for (var i = 1; i <= cantidad; i++) {
+            var div = $('<div>').addClass('form-group form-group-default required');
+            var form = $('<div>').addClass('row');
+            var label = $('<label>').attr('for', i).html(i + '.');
+            var input = $('<input>').addClass('form-control').attr('required', 'required')
+                    .attr('id', i).attr('step', 'any').attr('type', 'number');
+            form.append(label);
+            form.append(input);
+            div.append(form);
+            preguntas.append(div);
+        }
+        preguntas.append(save);
+
+        $('#preguntas').html(preguntas);
+        $('#btn-continue').remove();
+    })
+</script>
